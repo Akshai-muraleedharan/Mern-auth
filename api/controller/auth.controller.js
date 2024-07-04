8d647dda6d201852183bc8c85c518ca871510bad
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-07-04 16:49:10 +0100

    signin api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../Model/user.auth.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
 export const Signup = async(req,res,next) =>{
     
     const {username,email,password} = req.body;
@@ -15,4 +17,24 @@ try {
 
    
 
+}
+
+
+export const SignIn = async (req,res,next) =>{
+    const {email,password} = req.body;
+    try {
+        const validUser =await User.findOne({email})
+        if(!validUser) return next(errorHandler(404,"user not found"))
+        const validPassword =bcryptjs.compareSync(password, validUser.password)
+    if(!validPassword) return next(errorHandler(401,"wrong credential"))
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+    const {password:hashedPassword,...rest} =validUser._doc
+    const expiryDate = new Date(Date.now() + 360000)
+    res
+      .cookie('access_token', token,{httpOnly:true , expires:expiryDate})
+      .status(200) 
+      .json(rest)
+    } catch (error) {
+        next(error)
+    }
 }
