14cc5f4d627844ac1b8b6a88297ec962c1686f50
Akshai Dev <akshaimuraleedharan4@gmail.com>
2024-07-04 07:12:17 +0100

    Adding router to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Login from './pages/Login.jsx'
+import Profile from './pages/Profile.jsx'
+import Signup from './pages/Signup.jsx'
 function App() {
   return (
     <>
-    <div></div>
+    <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={ <About /> } />
+      <Route  path='/login' element={<Login /> }/>
+      <Route  path='/profil' element={<Profile /> }/>
+      <Route  path='/signup' element={<Signup /> }/>
+     </Routes>
+    </BrowserRouter>
     </>
   )
 }
